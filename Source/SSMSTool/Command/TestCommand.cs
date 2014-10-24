a4799a2c148efae6def30c6752cfb269d4bdd19e
leo.hecl <leo.hecl@ef.com>
2014-10-24 17:49:32 +0800

    vs plugin helper

---
+++ Source/SSMSTool/Command/TestCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE80;
+using SSMSTool.Common;
+
+namespace SSMSTool.Command
+{
+    public class TestCommand : CommandBase
+    {
+        public TestCommand(string caption, string tooltipText)
+            : base(caption, tooltipText)
+        {
+            
+        }
+
+        public override void Perform()
+        {
+            
+        }
+    }
+}
